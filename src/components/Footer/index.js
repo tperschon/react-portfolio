6d658e9356f113afddea51445332952a6ced8cce
TPerschon <timperschon@gmail.com>
2022-10-11 19:51:13 -0600

    Footer style prop

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,20 +1,20 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin, faStackExchange } from '@fortawesome/free-brands-svg-icons';
 
 function Footer() {
+    const style = { fontSize: "75px" }
     return (
         <footer className="footer">
             <div className="footer-container">
-                    <a href="https://github.com/tperschon">
-                        <FontAwesomeIcon icon={faGithub} />
+                    <a className="footer-link" href="https://github.com/tperschon">
+                        <FontAwesomeIcon icon={faGithub} style={style}/>
                     </a>
                     <a className="footer-link" href="https://www.linkedin.com/in/timothy-perschon-969250237/">
-                        <FontAwesomeIcon icon={faLinkedin} />
+                        <FontAwesomeIcon icon={faLinkedin} style={style}/>
                     </a>
                     <a className="footer-link" href="https://stackoverflow.com/users/18758369/tperschon">
-                        <FontAwesomeIcon icon={faStackExchange} />
+                        <FontAwesomeIcon icon={faStackExchange} style={style}/>
                     </a>
             </div>
         </footer>
